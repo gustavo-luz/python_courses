3e94a5c8445bcb4826212aca520bed993d9e5122
gustavo-luz <gustavo.luz.unb@gmail.com>
2021-08-25 10:39:47 -0300

    added iesb classes

---
+++ python_logic/iesb/iesb_classes.py
@@ -0,0 +1,60 @@
+
+"""
+file=open('arquivo.txt','w')
+file.write('Ola, como vc está?\n')
+file.write('bem e vc?')
+file.close
+"""
+
+
+#aula 17 
+
+"""
+#q3 c
+
+N= int(input('quantos? '))
+arq=open('num.txt','w')
+arq.close()
+
+for x in range(1,N+1):
+    arq=open('num.txt','a')
+    arq.write(str(x))
+    arq.write('\n')
+    arq.close
+
+
+
+#e
+
+N= int(input('quantos? '))
+arq=open('num.txt','w')
+
+
+for x in range(1,N):
+    arq.write(str(x))
+    arq.write('\n')
+    arq.close
+
+
+#q4 c
+
+import csv
+dado=[['1','maria','med'],['2','eric','eng']]
+with open('person.csv','w') as arq:
+    writer=csv.writer(arq,delimiter=';')
+    writer.writerows(dado)
+
+"""
+
+#q5 b
+with open('reais.txt','w') as arq:
+    num=3.4
+    arq.write("{0:.3f}\n".format(num))
+    num=2.1
+    arq.write("{0:.1f}\n".format(num))
+    num=4
+    arq.write("{0:.5f}\n".format(num))
+    num=1.3565
+    arq.write("{0:.2f}\n".format(num))
+    
+    
